ae5b4cbae5abcd2588a4085cfd34fe2b397a843b
gnos999 <s_armando@hotmail.com>
2015-07-13 03:11:44 -0400

    Despliegue en Heroku

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+var quizController = require('../controllers/quiz_controller');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Quiz' });
+});
+
+router.get('/quizes/question',quizController.question);
+router.get('/quizes/answer', quizController.answer);
+
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Author'});
+});
+
+module.exports = router;
